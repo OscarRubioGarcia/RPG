e6674bf540442e55690513bd6e18ae44d120e638
OscarRubioGarcia <takitow33@hotmail.com>
2017-03-23 00:15:07 +0100

    Desperation

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 var express = require('express'),
     server = express.createServer();
 
-var http = require('http').Server(server);
-
 server.use('/frameworks', express.static(__dirname + '/frameworks') );
 server.use('/src', express.static(__dirname + '/src') );
 server.use('/res', express.static(__dirname + '/res') );
@@ -10,11 +8,12 @@ server.use('/main.js', express.static(__dirname + '/main.js') );
 server.use('/project.json', express.static(__dirname + '/project.json') );
 
 server.get('/', function(req,res){
-    res.sendfile(__dirname + '/index.html');
+    res.sendfile('/index.html');
     console.log('Sent index.html');
 });
 
 server.get('/api/hello', function(req,res){
    res.send('Hello Cruel World');
 });
+
 server.listen(process.env.PORT || 3000);
