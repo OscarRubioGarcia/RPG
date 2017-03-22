d834340ce919b723591f547ddc431f3d2ef0910e
OscarRubioGarcia <takitow33@hotmail.com>
2017-03-23 00:17:43 +0100

    Despair

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-var express = require('express'),
-    server = express.createServer();
+var express = require('express');
+var app = express();
 
-server.use('/frameworks', express.static(__dirname + '/frameworks') );
-server.use('/src', express.static(__dirname + '/src') );
-server.use('/res', express.static(__dirname + '/res') );
-server.use('/main.js', express.static(__dirname + '/main.js') );
-server.use('/project.json', express.static(__dirname + '/project.json') );
+app.use('/frameworks', express.static(__dirname + '/frameworks') );
+app.use('/src', express.static(__dirname + '/src') );
+app.use('/res', express.static(__dirname + '/res') );
+app.use('/main.js', express.static(__dirname + '/main.js') );
+app.use('/project.json', express.static(__dirname + '/project.json') );
 
-server.get('/', function(req,res){
+app.get('/', function(req,res){
     res.sendfile('/index.html');
     console.log('Sent index.html');
 });
 
-server.get('/api/hello', function(req,res){
+app.get('/api/hello', function(req,res){
    res.send('Hello Cruel World');
 });
 
-server.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000);
