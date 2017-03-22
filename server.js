891fdf2841475e0198b69b332a976de1e8c22a0d
OscarRubioGarcia <takitow33@hotmail.com>
2017-03-22 23:43:01 +0100

    *****d program

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 var express = require('express'),
     server = express.createServer();
 
-app.use('/frameworks', express.static(__dirname + '/frameworks') );
-app.use('/src', express.static(__dirname + '/src') );
-app.use('/res', express.static(__dirname + '/res') );
-app.use('/main.js', express.static(__dirname + '/main.js') );
-app.use('/project.json', express.static(__dirname + '/project.json') );
+server.use('/frameworks', express.static(__dirname + '/frameworks') );
+server.use('/src', express.static(__dirname + '/src') );
+server.use('/res', express.static(__dirname + '/res') );
+server.use('/main.js', express.static(__dirname + '/main.js') );
+server.use('/project.json', express.static(__dirname + '/project.json') );
 
-app.get('/', function(req,res){
+server.get('/', function(req,res){
     res.sendfile('index.html');
     console.log('Sent index.html');
 });
 
-app.get('/api/hello', function(req,res){
+server.get('/api/hello', function(req,res){
    res.send('Hello Cruel World');
 });
-app.listen(process.env.PORT || 3000);
+server.listen(process.env.PORT || 3000);
